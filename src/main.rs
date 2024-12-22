1288b779d9bb72b15f5062e98a92ef460bd78035
paul <tabatabai@protonmail.com>
2024-12-22 22:58:07 +0100

    added num threads to command line

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,14 @@ fn main() {
     let m: usize = args[2].parse().unwrap();
     let max_len: usize = args[3].parse().unwrap();
     let prob_reject_worse: f32 = args[4].parse().unwrap();
+    let num_threads: usize = args[5].parse().unwrap();
 
     let g = Triangulation::from_random_appolonian_network(n);
     let h = Triangulation::from_random_appolonian_network(n);
     println!("{:?}", g);
 
     let mut handles = Vec::new();
-    for _ in 0..6 {
+    for _ in 0..num_threads {
         let g_tmp = g.clone();
         let h_tmp = h.clone();
         let handle = thread::spawn(move || anneal(g_tmp, h_tmp, m, max_len, prob_reject_worse));
@@ -40,7 +41,7 @@ fn main() {
         if done {
             break;
         }
-        sleep(Duration::from_secs(1));
+        sleep(Duration::from_millis(100));
     }
 
     // for _ in 0..10_000_000 {
